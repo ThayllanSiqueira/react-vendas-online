ea6bfe3b32d06d7c11218eea467d69f8abf4b60b
Thayllan Siqueira <thayllansiqueira@gmail.com>
2024-10-21 22:18:36 -0300

    feat: config pre-commit eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,20 @@ import js from '@eslint/js';
 import typescriptPlugin from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   js.configs.recommended,
   {
-    files: ['.js', '.jsx', '**/*.ts', '**/*.tsx'],
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: typescriptParser,
+      globals: {
+        document: 'readonly', // Definindo document como uma variável global somente leitura
+        window: 'readonly', // Você pode adicionar outras variáveis globais conforme necessário
+      },
     },
     plugins: {
       '@typescript-eslint': typescriptPlugin,
