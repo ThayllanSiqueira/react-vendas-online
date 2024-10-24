143841bc7354222519c0c88cf2ace092c7622842
Thayllan Siqueira <thayllansiqueira@gmail.com>
2024-10-24 08:35:14 -0300

    feat: create form insert product

--- src/modules/product/screens/ProductInsertScreen.tsx
+++ src/modules/product/screens/ProductInsertScreen.tsx
@@ -1,7 +1,26 @@
+import { Select } from 'antd';
+import { useEffect } from 'react';
+
+import Button from '../../../shared/components/buttons/button/Button';
+import Input from '../../../shared/components/inputs/input/input';
 import Screen from '../../../shared/components/screen/Screen';
+import { URL_CATEGORY } from '../../../shared/constants/urls';
+import { MethodsEnum } from '../../../shared/enums/methods.enum';
+import { useDataContext } from '../../../shared/hooks/useDataContext';
+import { useRequests } from '../../../shared/hooks/useRequests';
 import { ProductRoutesEnum } from '../routes';
+import { LimitedContainer } from '../styles/productinsert.style';
 
 const ProductInsert = () => {
+  const { categories, setCategories } = useDataContext();
+  const { request } = useRequests();
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      request(URL_CATEGORY, MethodsEnum.GET, setCategories);
+    }
+  }, []);
+
   const listBreadcrumb = [
     {
       title: 'HOME',
@@ -14,7 +33,30 @@ const ProductInsert = () => {
       title: 'INSERIR PRODUTOS',
     },
   ];
-  return <Screen listBreadcrumb={listBreadcrumb}>Insert Product</Screen>;
+
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+
+  return (
+    <Screen listBreadcrumb={listBreadcrumb}>
+      <LimitedContainer>
+        <Input margin="0 0 16px 0" title="Nome" placeholder="Nome" />
+        <Input margin="0 0 16px 0" title="Url Imagem" placeholder="Url Imagem" />
+        <Input margin="0 0 16px 0" title="Preço" placeholder="Preço" />
+        <Select
+          defaultValue="lucy"
+          style={{ width: '100%' }}
+          onChange={handleChange}
+          options={categories.map((category) => ({
+            value: `${category.id}`,
+            label: `${category.name}`,
+          }))}
+        />
+        <Button type="primary">Inserir Produto</Button>
+      </LimitedContainer>
+    </Screen>
+  );
 };
 
 export default ProductInsert;
