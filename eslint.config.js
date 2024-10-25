26231fef3ec5c9c9101981affb20d6fb824bc865
Thayllan Siqueira <thayllansiqueira@gmail.com>
2024-10-25 10:49:23 -0300

    feat: change eslint config ignore

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ import globals from 'globals';
 export default [
   js.configs.recommended,
   {
+    ignores: ['babel.config.js'],
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: typescriptParser,
