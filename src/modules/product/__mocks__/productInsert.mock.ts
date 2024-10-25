c0a7ba88856ce98b7132b02fbfbefbc7fe1a31d5
Thayllan Siqueira <thayllansiqueira@gmail.com>
2024-10-25 20:46:37 -0300

    test: unit in ProductInsert

--- src/modules/product/__mocks__/productInsert.mock.ts
+++ src/modules/product/__mocks__/productInsert.mock.ts
@@ -3,5 +3,5 @@ import { InsertProduct } from '../../../shared/dtos/InsertProduct.dto';
 export const mockProductInsert: InsertProduct = {
   image: 'http://image',
   name: 'MOCK_NAME',
-  price: 543,
+  price: 5.43,
 };
--- src/modules/product/screens/__tests__/ProductInsert.spec.tsx
+++ src/modules/product/screens/__tests__/ProductInsert.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { mockProductInsert } from '../../__mocks__/productInsert.mock';
 import ProductInsert from '../ProductInsertScreen';
@@ -14,12 +14,18 @@ jest.mock('../../../category/hooks/useCategory', () => ({
   }),
 }));
 
+let value = '';
+let type = '';
+
 jest.mock('../../hooks/useInsertProduct', () => ({
   useInsertProduct: () => ({
     product: mockProductInsert,
     loading: false,
     disableButton: false,
-    onChangeInput: jest.fn(),
+    onChangeInput: (e: React.ChangeEvent<HTMLInputElement>, x: string) => {
+      value = e.target.value;
+      type = x;
+    },
     handleInsertProduct: jest.fn(),
     handleChangeSelect: jest.fn(),
     handleOnClickCancel: jest.fn(),
@@ -36,5 +42,32 @@ describe('Test ProductInsert', () => {
     expect(getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_PRICE)).toBeDefined();
     expect(getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_SELECT)).toBeDefined();
     expect(getByTestId(ProductInsertTestIdEnum.PRODUCT_INSERT_CONTAINER)).toBeDefined();
+  });
+
+  it('should call onChangeInput in change name', () => {
+    const { getByTestId } = render(<ProductInsert />);
+    const input = getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_NAME);
+    fireEvent.change(input, { target: { value: 'MOCK_VALUE' } });
+
+    expect(value).toEqual('MOCK_VALUE');
+    expect(type).toEqual('name');
+  });
+
+  it('should call onChangeInput in change price', () => {
+    const { getByTestId } = render(<ProductInsert />);
+    const input = getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_PRICE);
+    fireEvent.change(input, { target: { value: '543' } });
+
+    expect(value).toEqual('5.43');
+    expect(type).toEqual('price');
+  });
+
+  it('should call onChangeInput in change image', () => {
+    const { getByTestId } = render(<ProductInsert />);
+    const input = getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_IMAGE);
+    fireEvent.change(input, { target: { value: 'http-image' } });
+
+    expect(value).toEqual('http-image');
+    expect(type).toEqual('image');
   });
 });
