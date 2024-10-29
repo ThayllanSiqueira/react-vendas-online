9211e8d8c0d96cb02339198a96c852dbac340edf
Thayllan Siqueira <thayllansiqueira@gmail.com>
2024-10-29 07:26:53 -0300

    feat: insert mask in order details

--- src/modules/order/screens/OrderDetailsScreen.tsx
+++ src/modules/order/screens/OrderDetailsScreen.tsx
@@ -3,7 +3,10 @@ import { useParams } from 'react-router-dom';
 
 import Screen from '../../../shared/components/screen/Screen';
 import { DisplayFlexJustifyCenter } from '../../../shared/components/styles/display.styled';
+import { insertMaskInCEP } from '../../../shared/functions/address';
+import { insertMaskInCpf } from '../../../shared/functions/cpf';
 import { convertNumberToMoney } from '../../../shared/functions/money';
+import { insertMaskInPhone } from '../../../shared/functions/phone';
 import ListOrderProduct from '../components/ListOrderProduct';
 import { useOrderDetail } from '../hooks/useOrderDetail';
 import { OrderRoutesEnum } from '../routes';
@@ -48,12 +51,12 @@ const OrderDetails = () => {
     {
       key: '3',
       label: 'Telefone',
-      children: `${order.user?.phone}`,
+      children: `${insertMaskInPhone(order.user?.phone)}`,
     },
     {
       key: '4',
       label: 'CPF',
-      children: `${order.user?.name}`,
+      children: `${insertMaskInCpf(order.user?.cpf)}`,
       span: 2,
     },
   ];
@@ -111,7 +114,7 @@ const OrderDetails = () => {
     {
       key: '5',
       label: 'CEP',
-      children: `${order.address?.cep}`,
+      children: `${insertMaskInCEP(order.address?.cep || '')}`,
     },
   ];
 
