d679c6cb410b7a0280189896946664a43ccff89f
Thayllan Siqueira <thayllansiqueira@gmail.com>
2024-10-29 13:43:09 -0300

    feat: insert delete in product

--- src/modules/product/screens/ProductScreen.tsx
+++ src/modules/product/screens/ProductScreen.tsx
@@ -1,51 +1,18 @@
 import { Input, type TableProps } from 'antd';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useMemo } from 'react';
 
 import Button from '../../../shared/components/buttons/button/Button';
 import Screen from '../../../shared/components/screen/Screen';
 import { DisplayFlexJustifyBetween } from '../../../shared/components/styles/display.styled';
 import { LimitedContainer } from '../../../shared/components/styles/limited.styled';
 import Table from '../../../shared/components/table/Table';
-import { URL_PRODUCT } from '../../../shared/constants/urls';
-import { MethodsEnum } from '../../../shared/enums/methods.enum';
 import { convertNumberToMoney } from '../../../shared/functions/money';
-import { useRequests } from '../../../shared/hooks/useRequests';
 import { ProductType } from '../../../shared/types/ProductType';
-import { useProductReducer } from '../../../store/reducers/productReducer/useProductReducer';
 import CategoryColumn from '../components/CategoryColumn';
 import TooltipImage from '../components/TooltipImage';
-import { ProductRoutesEnum } from '../routes';
+import { useProduct } from '../hooks/useProduct';
 
 const { Search } = Input;
-
-const columns: TableProps<ProductType>['columns'] = [
-  {
-    title: 'Id',
-    dataIndex: 'id',
-    key: 'id',
-    render: (_, product) => <TooltipImage product={product} />,
-  },
-  {
-    title: 'Nome',
-    dataIndex: 'name',
-    key: 'name',
-    sorter: (a, b) => a.name.localeCompare(b.name),
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Categoria',
-    dataIndex: 'category',
-    key: 'category',
-    render: (_, product) => <CategoryColumn category={product.category} />,
-  },
-  {
-    title: 'Preço',
-    dataIndex: 'price',
-    key: 'price',
-    render: (_, product) => <a>{convertNumberToMoney(product.price)}</a>,
-  },
-];
 
 const listBreadcrumb = [
   {
@@ -57,35 +24,43 @@ const listBreadcrumb = [
 ];
 
 const ProductScreen = () => {
-  const { products, setProducts } = useProductReducer();
-  const [productsFiltered, setProductsFiltered] = useState<ProductType[]>([]);
-  const { request } = useRequests();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setProductsFiltered([...products]);
-  }, [products]);
-
-  useEffect(() => {
-    request<ProductType[]>(URL_PRODUCT, MethodsEnum.GET, setProducts);
-  }, []);
-
-  const dataWithKeys = productsFiltered.map((product) => ({
-    ...product,
-    key: product.id,
-  }));
-
-  const handleOnClickInsert = () => {
-    navigate(ProductRoutesEnum.PRODUCT_INSERT);
-  };
-
-  const onSearch = (value: string) => {
-    if (!value) {
-      setProductsFiltered([...products]);
-    } else {
-      setProductsFiltered([...productsFiltered.filter((product) => product.name.includes(value))]);
-    }
-  };
+  const { dataWithKeys, handleOnClickInsert, onSearch, handleDeleteProduct } = useProduct();
+  const columns: TableProps<ProductType>['columns'] = useMemo(
+    () => [
+      {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+        render: (_, product) => <TooltipImage product={product} />,
+      },
+      {
+        title: 'Nome',
+        dataIndex: 'name',
+        key: 'name',
+        sorter: (a, b) => a.name.localeCompare(b.name),
+        render: (text) => <a>{text}</a>,
+      },
+      {
+        title: 'Categoria',
+        dataIndex: 'category',
+        key: 'category',
+        render: (_, product) => <CategoryColumn category={product.category} />,
+      },
+      {
+        title: 'Preço',
+        dataIndex: 'price',
+        key: 'price',
+        render: (_, product) => <a>{convertNumberToMoney(product.price)}</a>,
+      },
+      {
+        title: 'Action',
+        dataIndex: '',
+        key: 'x',
+        render: (_, product) => <a onClick={() => handleDeleteProduct(product.id)}>Deletar</a>,
+      },
+    ],
+    [],
+  );
 
   return (
     <Screen listBreadcrumb={listBreadcrumb}>
